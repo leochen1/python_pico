45f2db40f3203dea990160a728223ecb4f68b794
leochen <leocmc0723@hotmail.com>
2024-01-06 11:44:51 +0800

    Update pico11_2.py
    add alert

--- pico_w/pico11_2.py
+++ pico_w/pico11_2.py
@@ -1,6 +1,6 @@
 import network
 import time
-from machine import WDT
+from machine import WDT, Timer, ADC
 
 
 def connect(): 
@@ -29,6 +29,26 @@ def connect():
         print("OK")
         print(nic.ifconfig())
 
-connect()
 
+def alert():
+    print('要爆炸了!')
+    
+def callback1(t:Timer):
+    global start
+    sensor = ADC(4)    
+    vol = sensor.read_u16() * (3.3/65535)
+    temperature = 27 - (vol-0.706) / 0.001721
+    print(f'溫度:{temperature}')    
+    delta = time.ticks_diff(time.ticks_ms(), start)
+    print(delta)
+    #溫度超過24度,並且發送alert()的時間已經大於60秒
+    if temperature >= 24 and delta >= 60 * 1000:        
+        alert()
+        start = time.ticks_ms()#重新設定計時的時間
+        
 
+connect()       
+
+start = time.ticks_ms() - 60 * 1000 #應用程式啟動時,計時時間,先減60秒    
+time1 = Timer()
+time1.init(period=1000,callback=callback1)
