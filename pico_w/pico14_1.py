2f658a462ea731916d2cf10b40147ad380121b3a
leochen <leocmc0723@hotmail.com>
2024-01-27 14:41:15 +0800

    Update pico14_1.py
    add led, light, vr control

--- pico_w/pico14_1.py
+++ pico_w/pico14_1.py
@@ -1,12 +1,19 @@
-from machine import Timer,Pin
+from machine import Timer,Pin, ADC
+import time
 
 def fun10(t:Timer | None=None):
     print('10秒了')
     led.toggle()
 
-led = Pin(15, Pin.OUT)
-timer10 = Timer(period=10000, mode=Timer.PERIODIC, callback=fun10)
-fun10()
+def fun500ms(t:Timer):
+    print(f'light:{light.read_u16()}')
+    print(f'vr:{vr.read_u16()}')
 
+led = Pin(15, Pin.OUT)
+light = ADC(Pin(28))
+vr = ADC(Pin(27))
+timer10 = Timer(period=10000, mode=Timer.PERIODIC, callback=fun10)
+timer500ms = Timer(period=500, mode=Timer.PERIODIC, callback=fun500ms)
+fun10()
 
 
