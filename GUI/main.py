8fc7f3239ab71053bcbb5b44729f498dd59142e5
leochen <leocmc0723@hotmail.com>
2024-02-03 05:01:54 +0000

    add datetime

--- GUI/main.py
+++ GUI/main.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
-import time
+import datetime
 
 
 st.title("My Pico GUI")
 st.header("Leo House :red[溫度] 和 :blue[光線]")
-localtime = time.localtime()
 st.divider()
-st.write("現在時間: ", time.strftime("%Y-%m-%d %H:%M:%S", localtime))
+
+print(datetime.datetime.now())
+st.write(datetime.datetime.now())
