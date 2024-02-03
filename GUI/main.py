4c73d196ba83ca8198581786618973e2eff5c681
leochen <leocmc0723@hotmail.com>
2024-02-03 04:58:13 +0000

    add time

--- GUI/main.py
+++ GUI/main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
+import time
+
 
 st.title("My Pico GUI")
 st.header("Leo House :red[溫度] 和 :blue[光線]")
+localtime = time.localtime()
 st.divider()
+st.write("現在時間: ", time.strftime("%Y-%m-%d %H:%M:%S", localtime))
