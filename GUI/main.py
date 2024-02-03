9aa31a7d80eda248b72c993537663dafbefe1b2c
leochen <leocmc0723@hotmail.com>
2024-02-03 05:17:21 +0000

    加入台灣時區

--- GUI/main.py
+++ GUI/main.py
@@ -1,11 +1,16 @@
+import pytz
 import streamlit as st
 import pandas as pd
 import datetime
 
+taiwan_tz = pytz.timezone('Asia/Taipei')
+now = datetime.datetime.now(taiwan_tz)
+print(now)
+st.write(now)
 
 st.title("My Pico GUI")
 st.header("Leo House :red[溫度] 和 :blue[光線]")
 st.divider()
 
-print(datetime.datetime.now())
-st.write(datetime.datetime.now())
+
+
